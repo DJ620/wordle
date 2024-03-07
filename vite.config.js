a715f69f2093133813293c2e215ed1a8874714d0
DJ Hersh <DanielJHersh@gmail.com>
2024-03-06 21:03:51 -0800

    reverted

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "tailwindcss";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
+
 
 export default defineConfig({
   plugins: [react()],
-  root: "src",
-  build: {
-    outDir: "../dist",
-  },
   css: {
     postcss: {
       plugins: [tailwindcss()],
     },
-  },
-});
+  }
+})
