b6c3fd28ae26341113d5dda9d855f7438c705bc2
DJ Hersh <DanielJHersh@gmail.com>
2024-03-06 21:34:14 -0800

    switched to hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-       <Route path="/wordle" element={<Landing />} />
-       <Route path="/wordle/classic" element={<Home />} />
-       <Route path="/wordle/endless" element={<Home />} />
-       <Route path="/wordle/custom/:encodedWord/:customNumGuesses" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/classic" element={<Home />} />
+          <Route path="/endless" element={<Home />} />
+          <Route
+            path="/custom/:encodedWord/:customNumGuesses"
+            element={<Home />}
+          />
+        </Routes>
+      </HashRouter>
   );
 };
 
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -13,7 +13,7 @@ const LandingPage = () => {
     dispatch(setVersion("classic"));
     dispatch(setNumberOfGuesses(6));
     dispatch(setNumberOfLetters(5));
-    navigate("/wordle/classic");
+    navigate("/classic");
   };
 
   return (
